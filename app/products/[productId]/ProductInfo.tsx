73363fbbba0ab7ba7cb2fbb58014b57806183e5a
nadir <Jana7Gamer@gmail.com>
2025-08-22 19:10:36 +0100

    changed price and name size

--- app/products/[productId]/ProductInfo.tsx
+++ app/products/[productId]/ProductInfo.tsx
@@ -34,7 +34,6 @@ const ProductInfo = (props: { product: ProductType }) => {
           )}
           loading="lazy"
         />
-        {/* <ImagePreview product={Product} /> */}
 
         {/* Thumbnails */}
         <Thumbnails
@@ -51,12 +50,10 @@ const ProductInfo = (props: { product: ProductType }) => {
             "sm:col-span-2 sm:row-start-2 sm:px-0"
           )}
         >
-          <h1 className="text-2xl text-natural-700 font-bold">
-            {Product.name}
-          </h1>
+          <h1 className="text-xl text-natural-700 font-bold">{Product.name}</h1>
           <div className="row-start-2 col-span-2 flex gap-1 items-center">
             <svg
-              className="h-4 w-auto fill-natural-600"
+              className="h-3 w-auto fill-natural-600"
               xmlns="http://www.w3.org/2000/svg"
               id="Layer_1"
               data-name="Layer 1"
@@ -71,7 +68,7 @@ const ProductInfo = (props: { product: ProductType }) => {
                 d="M1085.73,895.8c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.33v-135.2l292.27-62.11c20.06-44.47,33.32-92.75,38.4-143.37l-330.68,70.27V66.13c-50.67,28.45-95.67,66.32-132.25,110.99v403.35l-132.25,28.11V0c-50.67,28.44-95.67,66.32-132.25,110.99v525.69l-295.91,62.88c-20.06,44.47-33.33,92.75-38.42,143.37l334.33-71.05v170.26l-358.3,76.14c-20.06,44.47-33.32,92.75-38.4,143.37l375.04-79.7c30.53-6.35,56.77-24.4,73.83-49.24l68.78-101.97v-.02c7.14-10.55,11.3-23.27,11.3-36.97v-149.98l132.25-28.11v270.4l424.53-90.28Z"
               />
             </svg>
-            <h3 className="text-base text-natural-600 font-medium">
+            <h3 className="text-sm text-natural-600 font-medium">
               {Product.price}
             </h3>
           </div>
